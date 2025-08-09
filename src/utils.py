b7b3d6618b75dcf729bcdbcfb02f9c5eef177c93
Atharva Gholap <atharvagholap205@gamil.com>
2025-08-09 17:38:33 +0530

    Data Transformation done

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,15 @@
-import sys # sys is used for getting detailed information about the exception
+import os
+import sys
+import pickle
 
+from src.exception import CustomException
 
-def error_message_detail(error,error_detail:sys):  #generates custom error message
-    _,_,exc_tb=error_detail.exc_info()  #the error_detail.exc_info() will give 3 info we are only interested in 3 one so we put _,_,for the first 2
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
 
-    file_name=exc_tb.tb_frame.f_code.co_filename #this will give the filename in which error has occured
-
-    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
-
-    return error_message
-
-
-
-class CustomException(Exception):  #this class inherits python exceptions and more to it
-    def __init__(self,error_message,error_detail:sys):
-        
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-
-    def __str__(self):  #makes the error printable
-        return self.error_message
-    
-
-
-# error_message => The original error (e.g., "division by zero")
-# self.error_message => The full message with filename, line number, and the original error
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+    except Exception as e :
+        raise CustomException(e,sys)
